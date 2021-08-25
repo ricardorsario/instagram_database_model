ebaabee39a033c3bf55f4ce2c7b39ad688314a61
ricardorsario <ricardorsario@gmail.com>
2021-08-25 18:40:20 +0000

    upload all

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,31 +8,83 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, primary_key=True)
+    name = Column(String)
+    username = Column(String, unique = True)
+    mail = Column(String, unique = True)
+    password = Column(String)
+    description = Column(String, nullable = True)
+    profile_img = Column(String)
+    tlf = Column(String)
+    age = Column(String, nullable = True)
+    gender = Column(String, nullable = True)
 
-    def to_dict(self):
-        return {}
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+class Followers(Base):
+    __tablename__ = 'Followers'
+    
+    followers_id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer)
+    user_to_id = Column(Integer)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_link = relationship("User", back_populates="parent", uselist=False)
+
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    post_id = Column(Integer, primary_key=True)
+    text = Column(String, nullable = True)
+    location = Column(String)
+    time = Column(String)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_link = relationship("User", back_populates="parent", uselist=False)
+    media_id = Column(Integer)
+    media_link = relationship("Media", back_populates="parent", uselist=False)
+    like_id = Column(Integer)
+    like_link = relationship("Like", back_populates="parent", uselist=False)   
+
+
+class Media(Base):
+    __tablename__ = 'media'
+
+    media_id = Column(Integer, primary_key=True)
+    url = Column(String)
+    type_media = Column(Enum) 
+
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post_link = relationship("Post", back_populates="parent", uselist=False)
+    
+
+class Like(Base):
+    __tablename__ = 'like'
+
+    like_id = Column(Integer, primary_key=True)
+    
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_link = relationship("User", back_populates="parent", uselist=False)
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    
+    comment_id = Column(Integer, primary_key=True)
+    text = Column(String)
+
+    post_id = Column(Integer, ForeignKey('post_id'))
+    post_link = relationship("Post", back_populates="parent", uselist=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_link = relationship("User", back_populates="parent", uselist=False)
+    like_id = Column(Integer, ForeignKey('like.id'))
+    like_link = relationship("Like", back_populates="parent", uselist=False)
+    
+
+def to_dict(self):
+    return {}
+
+
+render_er(Base, 'diagram.png')
